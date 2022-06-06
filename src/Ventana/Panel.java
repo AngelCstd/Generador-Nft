3291530d617a42bb5d74a26cdda7a0782ae7ebae
Angel Castañeda <luisacast.29@gmail.com>
2022-06-05 23:22:13 -0500

    "Version con gdraw, fallas al imprimir"

--- src/Ventana/Panel.java
+++ src/Ventana/Panel.java
@@ -11,11 +11,68 @@ public class Panel extends JPanel implements Printable {
     public Panel() {
         iniciarPanel();
     }
+    @Override
+    public void paint(Graphics g){
+        super.paint(g);
+
+        ImageIcon imagen = randomCuerpo();
+        g.drawImage(imagen.getImage(), 0, 0, 500, 500, this);
+        ImageIcon imagen1 = randomOjos();
+        g.drawImage(imagen1.getImage(), 0, 0, 500, 500, this);
+        ImageIcon imagen2 = randomBoca();
+        g.drawImage(imagen2.getImage(), 0, 0, 500, 500, this);
+    }
 
     private void iniciarPanel() {
         this.setLayout(null);
         this.setSize(500, 500);
         this.setVisible(false);
+    }
+    private ImageIcon randomCuerpo() {
+        ImageIcon fondo = new ImageIcon(getClass().getResource("fondo.png"));
+        ImageIcon fondo1 = new ImageIcon(getClass().getResource("fondo1.png"));
+        ImageIcon fondo2 = new ImageIcon(getClass().getResource("fondo2.png"));
+        int random = (int) (Math.random() * 3);
+        switch (random) {
+            case 0:
+                return fondo;
+            case 1:
+
+                return fondo1;
+            case 2:
+
+                return fondo2;
+        }
+        return null;
+    }
+    private ImageIcon randomOjos() {
+        ImageIcon ojos = new ImageIcon(getClass().getResource("ojos.png"));
+        ImageIcon ojos1 = new ImageIcon(getClass().getResource("ojos1.png"));
+        ImageIcon ojos2 = new ImageIcon(getClass().getResource("ojos2.png"));
+        int random1 = (int) (Math.random() * 3);
+        switch (random1) {
+            case 0:
+                return ojos;
+            case 1:
+                return ojos1;
+            case 2:
+               
+               return ojos2;
+        }
+        return null;
+    }
+
+    private ImageIcon randomBoca() {
+        ImageIcon boca = new ImageIcon(getClass().getResource("boca.png"));
+        ImageIcon boca1 = new ImageIcon(getClass().getResource("boca1.png"));
+        int random3 = (int) (Math.random() * 2);
+        switch (random3) {
+            case 0:
+                return boca;
+            case 1:
+                return boca1;
+        }
+        return null;
     }
 
     @Override
--- src/Ventana/VentanaFrame.java
+++ src/Ventana/VentanaFrame.java
@@ -10,7 +10,6 @@ public class VentanaFrame extends JFrame {
 
     Panel panel;
     JButton buttonmostrar, buttonimprimir;
-    
 
     public VentanaFrame() {
 
@@ -49,9 +48,7 @@ public class VentanaFrame extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 if (!panel.isVisible()) {
-                    randomOjos();
-                    randomBoca();
-                    randomCuerpo();
+                    panel.repaint();
                     panel.setVisible(true);
                 } else {
                     panel.removeAll();
@@ -59,14 +56,13 @@ public class VentanaFrame extends JFrame {
                 }
             }
         });
-        
-        
+
         buttonimprimir.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 PrinterJob job = PrinterJob.getPrinterJob();
                 job.setPrintable(panel);
-                if(job.printDialog()){
+                if (job.printDialog()) {
                     try {
                         job.print();
                     } catch (PrinterException ex) {
@@ -78,79 +74,4 @@ public class VentanaFrame extends JFrame {
 
     }
 
-   
-    
-    
-    private void randomCuerpo() {
-        JLabel fondo = new JLabel(new ImageIcon("fondo.png"));
-        JLabel fondo1 = new JLabel(new ImageIcon("fondo1.png"));
-        JLabel fondo2 = new JLabel(new ImageIcon("fondo2.png"));
-        int random = (int) (Math.random() * 3);
-        switch (random) {
-            case 0:
-                fondo.setBounds(0, 0, 500, 500);
-                panel.add(fondo);
-                break;
-            case 1:
-                fondo1.setBounds(0, 0, 500, 500);
-                panel.add(fondo1);
-                break;
-            case 2:
-                fondo2.setBounds(0, 0, 500, 500);
-                panel.add(fondo2);
-                break;
-
-        }
-    }
-
-    private void randomOjos() {
-        JLabel ojos = new JLabel(new ImageIcon("ojos.png"));
-        JLabel ojos1 = new JLabel(new ImageIcon("ojos1.png"));
-        JLabel ojos2 = new JLabel(new ImageIcon("ojos2.png"));
-        int random1 = (int) (Math.random() * 3);
-        switch (random1) {
-            case 0:
-                ojos.setBounds(0, 0, 500, 500);
-                panel.add(ojos);
-                break;
-            case 1:
-                ojos1.setBounds(0, 0, 500, 500);
-                panel.add(ojos1);
-                break;
-            case 2:
-                int random2 = (int) (Math.random() * 3);//Le da menor porcentaje de que salga los cuernitos
-                switch (random2) {
-                    case 0:
-                        ojos.setBounds(0, 0, 500, 500);
-                        panel.add(ojos);
-                        break;
-                    case 1:
-                        ojos1.setBounds(0, 0, 500, 500);
-                        panel.add(ojos1);
-                        break;
-                    case 2:
-                        ojos2.setBounds(0, 0, 500, 500);
-                        panel.add(ojos2);
-                        break;
-
-                }
-                break;
-        }
-    }
-
-    private void randomBoca() {
-        JLabel boca = new JLabel(new ImageIcon("boca.png"));
-        JLabel boca1 = new JLabel(new ImageIcon("boca1.png"));
-        int random3 = (int) (Math.random() * 2);
-        switch (random3) {
-            case 0:
-                boca.setBounds(0, 0, 500, 500);
-                panel.add(boca);
-                break;
-            case 1:
-                boca1.setBounds(0, 0, 500, 500);
-                panel.add(boca1);
-                break;
-        }
-    }
 }
